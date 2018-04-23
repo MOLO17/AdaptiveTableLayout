fdecd4d1d9bfc65542f0c3cd430d9988c25893bf
Federico Monti <federico.monti@molo17.com>
2018-04-23 11:17:07 +0200

    Added OnRowHeaderLongClickListener

--- library/src/main/java/com/cleveroad/adaptivetablelayout/AdaptiveTableLayout.java
+++ library/src/main/java/com/cleveroad/adaptivetablelayout/AdaptiveTableLayout.java
@@ -1472,6 +1472,8 @@ public class AdaptiveTableLayout extends ViewGroup implements ScrollHelper.Scrol
                     onItemClickListener.onItemLongClick(viewHolder.getRowIndex(), viewHolder.getColumnIndex());
                 } else if (viewHolder.getItemType() == ViewHolderType.FIRST_HEADER) {
                     onItemClickListener.onLeftTopHeaderLongClick();
+                } else if (viewHolder.getItemType() == ViewHolderType.ROW_HEADER) {
+                    onItemClickListener.onRowHeaderLongClick(viewHolder.getRowIndex());
                 }
             }
         }
