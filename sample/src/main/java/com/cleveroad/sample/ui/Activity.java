3bbe74a05aa8a8e43e3a318ef7aebcc4fc2ff6d5
Michelangelo Altamore <michelangelo.altamore@molo17.com>
2019-05-02 15:25:25 +0200

    Migrate to AndroidX version 1.0.0

--- sample/src/main/java/com/cleveroad/sample/ui/Activity.java
+++ sample/src/main/java/com/cleveroad/sample/ui/Activity.java
@@ -1,6 +1,6 @@
 package com.cleveroad.sample.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cleveroad.sample.R;
